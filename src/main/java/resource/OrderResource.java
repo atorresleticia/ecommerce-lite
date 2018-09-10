b4a3be7d19d324728f8e284faf97952bafb086da
Leticia Torres <atorresleticia@gmail.com>
2018-09-10 10:25:43 -0300

    changed logic on average

--- src/main/java/resource/OrderResource.java
+++ src/main/java/resource/OrderResource.java
@@ -1,5 +1,8 @@
 package resource;
 
+import java.util.List;
+
+import javax.sound.sampled.Line;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 
 import dao.ItemDao;
 import dao.OrderDao;
+import model.Item;
 import model.Order;
 
 @Path("/order")
@@ -27,10 +31,22 @@ public class OrderResource {
 			@DefaultValue("0") @QueryParam("profit") float profit) {
 
 		Order order = new Order();
-
+		
 		order.setItems(ItemDao.getItems());
+		
+
+		List<Item> items = order.getItems();
+		
+		int totalProducts = 0;
+		
+		for (Item item : items) {
+			totalProducts += item.getQuantity();			
+		}
+		
+		System.out.println(totalProducts);
+		
 		order.setExpenses(expenses);
-		order.setAverage(expenses / order.getItems().size());
+		order.setAverage(expenses / totalProducts);
 		order.setProfit(profit);
 
 		return OrderDao.createOrder(order);
